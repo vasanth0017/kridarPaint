303570f79d452fdc150c68cbf4383626f7583ac4
vasanth0017 <eswarvasanth17@gmail.com>
2025-03-09 19:29:34 +0530

    refactor(*)dashboard change

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -24,15 +24,15 @@ export default async function HomePage({ children }: any) {
   if (!session) {
     return redirect('/sign-in')
   }
-    if (user?.email !== "eswarvasanth17@gmail.com") {
-    return (
-      <div className="flex items-center justify-center">
-        <h1 className="flex items-center justify-center text-4xl w-full h-full">
-          This page access only for Management Team
-        </h1>
-      </div>
-    );
-  }
+  //   if (user?.email !== "eswarvasanth17@gmail.com") {
+  //   return (
+  //     <div className="flex items-center justify-center">
+  //       <h1 className="flex items-center justify-center text-4xl w-full h-full">
+  //         This page access only for Management Team
+  //       </h1>
+  //     </div>
+  //   );
+  // }
   return (
     <>
       <SidebarProvider>
